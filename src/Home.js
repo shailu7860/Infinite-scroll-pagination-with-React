e7ea0f08fb84dd33346b57871d2b26193aaad99c
shailu7860 <shailendramis01@gmail.com>
2022-05-23 18:27:39 +0530

    some design

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,8 @@
 import './style/App.css';
 import Navbaar from './components/Navbar';
+import MediaCard from './components/Card';
+import Image1 from './icons/designer.svg';
+import Image2 from './icons/frontend.svg';
 
 function Home() {
   return (
@@ -8,35 +11,64 @@ function Home() {
     <div className="App">
       <Navbaar />
       <div className="App-header">
-    
-        
+
+
         <h1 data-testid="child" className='p_tag' >
           <b>
-          Designer & Front-end Developer.
+            Designer & Front-end Developer.
           </b>
         </h1>
         <p className='p_tag2'>
-          
+
           I design and code beautifully simple things, and I love what I do.
-       
+
         </p>
-           
-      
-      <img src={"https://mattfarley.ca/img/mf-avatar.svg"} className="App-logo" alt="logo" />
-       </div>
+
+
+        <img src={"https://mattfarley.ca/img/mf-avatar.svg"} className="App-logo" alt="logo" />
+      </div>
       <div className='about_me'>
         <img src='https://mattfarley.ca/img/hero.svg' className='table_img' alt="table_image" />
-        </div>
-        <div className='summary'>
-          <div className='summary2'>
+      </div>
+      <div className='summary'>
+        <div className='summary2'>
+
           <h2 className='summary_text'>Hi, I’m Shailendra. Nice to meet you.</h2>
+
           <p className='summary_text2'>Since beginning my journey as a WEB Developer nearly 1 years ago, I've done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.
           </p>
-          </div>
-        
+          
+        </div>
+        <div className='about-card' style={{ width: "100%",display:'flex',justifyContent: "space-around",
+  position: "absolute",
+  top: "85rem",
+  
+  }}>
+
+           <MediaCard image={Image1} tilte={"Designer"} desc={"I value simple content structure, clean design patterns, and thoughtful interactions."} type={"Things I enjoy designing:"} type2={"UX, UI, Web, Mobile, Apps, Logos"} tools={" Design Tools:"} tools2={" Balsamiq Mockups Figma Invision MarvelPen & Paper Sketch Webflow Zeplin "} />
+                   
+           <MediaCard  image={Image2} tilte={"Front-end Developer"} desc={"II like to code things from scratch, and enjoy bringing ideas to life in the browser."} type={"Languages I speak:"} type2={"HTML, CSS, Java-Script, J-Quary, React-Js, Next-Js"} tools={"Dev Tools:"} tools2={
+           <ul style={{listStyle: "none" ,padding:"10px"}}>
+             <li>Visual Studio</li>
+             <li>Bootstrap</li>
+             <li>Material Ui</li>
+             <li>Chakra Ui</li>
+             <li>Reactstrap</li>
+           </ul> } />
+
+
+
+
+
+
+
+
          </div>
-        
-      
+
+      </div>
+
+
+
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import './Contact.css'
+
+export default function MediaCard(props) {
+    return (
+        <Card className='Card' sx={{ maxWidth: 345, paddingTop:"60px" }}>
+            <CardMedia
+                component="img"
+                height="140"
+                image={props.image}
+                alt="green iguana"
+                style={{width: "fit-content",
+                    position: "absolute",
+                    left: "140px",
+                    top: "20px"}}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                   {props.tilte} 
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {props.desc}
+                    
+                </Typography>
+
+                <Typography variant="body2" color="black">
+                    {props.type}
+                    
+                </Typography>
+
+                <Typography style={{display:'flex', justifyContent:'center'}} color="text.secondary" >
+                    {props.type2}
+                    
+                </Typography>
+
+                <Typography variant="body2" color="black">
+                    {props.tools}
+               
+                </Typography>
+
+                <Typography style={{display:'inline-flex',justifyContent:'center'}}  color="text.secondary">
+                {props.tools2}
+                   
+                </Typography>
+
+
+            </CardContent>
+
+         
+        </Card>
+    );
+}
